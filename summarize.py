e1259a10ee81d8f2dae1ab12cf303542c8fc6685
a-b-v-k <vijaykrishnaabv@gmail.com>
2023-03-05 20:29:47 +0530

    tensorflow import fix

--- summarize.py
+++ summarize.py
@@ -1,11 +1,11 @@
 from datetime import datetime
-from transformers import BartTokenizer, TFBartForConditionalGeneration, pipeline
+from transformers import BartTokenizer, BartForConditionalGeneration, pipeline
 from Utils import fetch_article_text, count_tokens
 import re
 from nltk.tokenize import sent_tokenize
 
 tokenizer = BartTokenizer.from_pretrained('facebook/bart-large-cnn')
-model = TFBartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
+model = BartForConditionalGeneration.from_pretrained('facebook/bart-large-cnn')
 
 def bart_summarize(text: str):
 
